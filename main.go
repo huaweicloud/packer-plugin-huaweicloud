2e8508e89f972ffee148a9007676cf91cc4851f6
ShiChangkuo <shichangkuo90@gmail.com>
2021-04-26 17:25:53 +0800

    upgrade plugin version to 0.4.0-rc1

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "0.3.0"
+	Version = "0.4.0"
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
+	VersionPrerelease = "rc1"
 
 	// PluginVersion is used by the plugin set to allow Packer to recognize
 	// what version this plugin is.
