461e0b230cf5109b09101c7fc50b9380a55a84f6
shichangkuo <shichangkuo90@gmail.com>
2022-12-14 12:59:04 +0800

    feat: get the cloud name from auth_url (#54)

--- builder/ecs/endpoint.go
+++ builder/ecs/endpoint.go
@@ -1,6 +1,10 @@
 package ecs
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
 
 type ServiceCatalog struct {
 	Name  string
@@ -24,8 +28,7 @@ var serviceEndpoints = map[string]ServiceCatalog{
 }
 
 // GetServiceEndpoint try to get the endpoint from customizing map
-func GetServiceEndpoint(srv, region string) string {
-	var cloud string = "myhuaweicloud.com"
+func GetServiceEndpoint(cloud, srv, region string) string {
 	// get the endpoint from build-in service catalog
 	catalog, ok := serviceEndpoints[srv]
 	if !ok {
@@ -39,4 +42,31 @@ func GetServiceEndpoint(srv, region string) string {
 		ep = fmt.Sprintf("https://%s.%s.%s/", catalog.Name, region, cloud)
 	}
 	return ep
+}
+
+func GetCloudFromAuth(auth string) (string, error) {
+	var cloud string
+
+	u, err := url.Parse(auth)
+	if err != nil {
+		return "", err
+	}
+
+	// the parsed Host in host:port format, get rid of the port
+	hosts := strings.SplitN(u.Host, ":", 2)
+
+	subhosts := strings.Split(hosts[0], ".")
+	total := len(subhosts)
+	if total == 3 {
+		// without region: iam.myhuaweicloud.com
+		cloud = strings.Join(subhosts[1:], ".")
+	} else if total > 3 {
+		// with region: iam.cn-north-1.myhuaweicloud.com
+		// iam.eu-west-0.prod-cloud-ocb.orange-business.com
+		cloud = strings.Join(subhosts[2:], ".")
+	} else {
+		return "", fmt.Errorf("the auth_url is invalid")
+	}
+
+	return cloud, nil
 }
