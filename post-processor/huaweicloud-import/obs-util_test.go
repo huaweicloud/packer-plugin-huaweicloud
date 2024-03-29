10328cb692a2df3e638d70b9f146d1ee7dbe7595
shichangkuo <shichangkuo90@gmail.com>
2024-03-29 14:49:06 +0800

    feat: post processor support quick import

---
+++ post-processor/huaweicloud-import/obs-util_test.go
@@ -0,0 +1,27 @@
+package huaweicloudimport
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/huaweicloud/huaweicloud-sdk-go-obs/obs"
+)
+
+func TestUploadFileToObject(t *testing.T) {
+	region := os.Getenv("HW_REGION_NAME")
+	ak := os.Getenv("HW_ACCESS_KEY")
+	sk := os.Getenv("HW_SECRET_KEY")
+	obsEndpoint := fmt.Sprintf("https://obs.%s.myhuaweicloud.com", region)
+	envProxyConfigure := obs.WithProxyFromEnv(true)
+
+	client, err := obs.New(ak, sk, obsEndpoint, obs.WithSignature("OBS"), envProxyConfigure)
+	if err != nil {
+		t.Fatalf("failed to create OBS client: %s", err)
+	}
+
+	err = uploadFileToObject(client, "image-test", "packer-import-test.raw", "./source-image-test.raw")
+	if err != nil {
+		t.Fatalf("failed to upload file: %s", err)
+	}
+}
